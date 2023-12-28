a963a73cab97f8abc5ebad2e0661811083dca924
oscar <o.lrodriguez1195@gmail.com>
2023-12-27 21:55:01 -0600

    feat(app):project_init

---
+++ src/PollitosPorciones.jsx
@@ -0,0 +1,8 @@
+export const PollitosPorciones = () => {
+  return (
+    <>
+      <h1>PollitosPorciones</h1>
+      <hr />
+    </>
+  );
+};
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { PollitosPorciones } from './PollitosPorciones';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PollitosPorciones />
+  </React.StrictMode>,
+);
